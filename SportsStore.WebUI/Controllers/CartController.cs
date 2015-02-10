4f2cd88400a7dd9ffdbc84a6c36787e2857c0a90
gschatte <gerry@schattehome.com>
2015-02-09 18:46:20 -0600

    More changes

---
+++ SportsStore.WebUI/Controllers/CartController.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private IProductRepository repository;
+        
+        public CartController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public RedirectToRouteResult AddToCart(int productID, string returnURL)
+        {
+
+        }
+    }
+}
